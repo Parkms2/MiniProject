37098cda8427a43dc2e0e6e257720c3ae874bf9b
박문수 <irusias@naver.com>
2018-10-07 00:21:29 +0900

    bb

---
+++ miniProject/miniProject/main.cpp
@@ -0,0 +1,14 @@
+#include"game.h"
+Game* g_game = 0;
+
+int main(int argc, char*args[]) {
+	g_game = new Game();
+	g_game->init("miniProject", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, false);
+	while (g_game->running()) {
+		g_game->habdleEvents();
+		g_game->update();
+		g_game->render();
+	}
+	g_game->clean();
+	return 0;
+}
